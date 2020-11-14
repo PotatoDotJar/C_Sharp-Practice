db0ad3e7c51278846086ecaf3e43288623e40b40
RJ <rmnader@svsu.edu>
2020-11-14 14:10:22 -0500

    Switched to using .NET Framework.

---
+++ DotNetFrameworkRefresh/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DotNetFrameworkRefresh.Models
+{
+    [Table("cars")]
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string IdentificationNumber { get; set; }
+        public int Mileage { get; set; }
+    }
+}
